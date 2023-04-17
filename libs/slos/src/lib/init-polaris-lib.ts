da16b9ab788b8e22cb9dc60782e9320d5bd8b875
István Kiss <kissisti98@outlook.com>
2023-04-17 15:06:37 +0200

    Remove multi-elasticity-strategy

--- libs/slos/src/lib/init-polaris-lib.ts
+++ libs/slos/src/lib/init-polaris-lib.ts
@@ -1,6 +1,5 @@
 import { PolarisRuntime } from '@polaris-sloc/core';
 import { AvailableCpuPerPodMetricMapping } from './metrics/available-cpu-per-pod-metric.prm';
-import {MultiElasticityStrategy} from './elasticity/multi-elasticity-strategy.prm';
 import {CpuUtilizationSloMapping} from './slo-mappings/cpu-utilization.slo-mapping.prm';
 
 /**
@@ -10,10 +9,6 @@ export function initPolarisLib(polarisRuntime: PolarisRuntime): void {
   polarisRuntime.transformer.registerObjectKind(
     new AvailableCpuPerPodMetricMapping().objectKind,
     AvailableCpuPerPodMetricMapping
-  );
-  polarisRuntime.transformer.registerObjectKind(
-    new MultiElasticityStrategy().objectKind,
-    MultiElasticityStrategy
   );
   polarisRuntime.transformer.registerObjectKind(
     new CpuUtilizationSloMapping().objectKind,
