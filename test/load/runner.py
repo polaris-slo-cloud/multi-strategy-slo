63f2db52e68820d50b64500ae11e4790dc5223da
István Kiss <kissisti98@outlook.com>
2023-07-04 22:34:08 +0200

    fix test start

--- test/load/runner.py
+++ test/load/runner.py
@@ -3,7 +3,7 @@ import sys
 import load_tester as test
 
 data = [499, 499, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1400, 1300, 1200, 1100, 1000, 900, 800, 700, 600, 501]
-data = [499, 550, 450, 400, 800, 880, 700, 660, 1500, 1520, 1510, 500, 480, 520, 450, 400, 360, 300, 600, 550, 500, 470, 519]
+data = [499, 499, 550, 450, 400, 800, 880, 700, 660, 1500, 1520, 1510, 500, 480, 520, 450, 400, 360, 300, 600, 550, 500, 470, 519]
 
 def test_runner(slo_test):
 	try:
@@ -21,4 +21,3 @@ test_runner(test.SloTest('Random Decision', 'resource-consumer', ['resource-cons
 test_runner(test.SloTest('Round Robin Decision', 'resource-consumer', ['resource-consumer.yaml', 'round.yaml'], 'round'))
 test_runner(test.SloTest('Priority Decision', 'resource-consumer', ['resource-consumer.yaml', 'priority.yaml'], 'priority'))
 test_runner(test.SloTest('Threshold Decision', 'resource-consumer', ['resource-consumer.yaml', 'threshold.yaml'], 'threshold'))
-
