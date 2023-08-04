c2a3839fa6475d192960ed453cd18a96bcab1153
István Kiss <kissisti98@outlook.com>
2023-08-04 22:31:47 +0200

    refactor avg cpu utilization calculation

--- apps/demo-average-cpu-utilization-metric-controller/src/app/metrics/average-cpu-utilization/average-cpu-utilization.metric-source.ts
+++ apps/demo-average-cpu-utilization-metric-controller/src/app/metrics/average-cpu-utilization/average-cpu-utilization.metric-source.ts
@@ -1,10 +1,10 @@
 import {
   ComposedMetricSourceBase,
-  Join,
+  Join, LabelFilter,
   LabelFilters,
   MetricsSource,
   OrchestratorGateway,
-  Sample,
+  Sample, TimeInstantQuery,
 } from '@polaris-sloc/core';
 import {AverageCpuUtilization, AverageCpuUtilizationParams} from '@org/slos';
 import {Observable} from 'rxjs';
@@ -40,36 +40,57 @@ export class AverageCpuUtilizationMetricSource extends ComposedMetricSourceBase<
   }
 
   private async getAvgCpuUtilization(): Promise<number> {
-    const resourceFilter = LabelFilters.equal('resource', 'cpu');
-    const podFilter = LabelFilters.regex('pod', `${this.params.sloTarget.name}.*`);
-    const deploymentFilter = LabelFilters.equal('deployment', this.params.sloTarget.name);
-
-    const replicas = this.metricsSource.getTimeSeriesSource()
-      .select<number>('kube', 'deployment_spec_replicas')
-      .filterOnLabel(deploymentFilter)
-      .minByGroup();
-
-    const cpuLimit = this.metricsSource.getTimeSeriesSource()
-      .select<number>('kube', 'pod_container_resource_limits')
-      .filterOnLabel(podFilter)
-      .filterOnLabel(resourceFilter)
-      .minByGroup()
-      .multiplyBy(replicas);
-
-    const cpuMillis = this.metricsSource.getTimeSeriesSource()
-      .select<number>('polaris', 'composed_metrics_polaris_slo_cloud_github_io_v1_cpu_load')
-      .filterOnLabel(LabelFilters.regex('target_name', `${this.params.sloTarget.name}.*`))
-      .sumByGroup();
-
-    const cpuUsage = cpuMillis.divideBy(cpuLimit).multiplyBy(100);
+    const cpuUsage = this.queryCpuUsage();
 
     const result = await cpuUsage.execute();
     const value = result.results[0]?.samples[0].value;
 
+    return this.clampResult(value);
+  }
+
+  private clampResult(value: number) {
     if (value == null || isNaN(value)) {
       throw Error(`Invalid query result: ${value}`)
     }
 
-    return Math.ceil(Math.min(100, Math.max(0, value)));
+    const lowerBound = Math.max(0, value);
+    const upperBound = Math.min(100, lowerBound);
+    return Math.ceil(upperBound);
+  }
+
+  private queryCpuUsage(): TimeInstantQuery<number> {
+    const cpuLimit = this.queryCpuLimit();
+    const cpuLoad = this.queryCpuLoad();
+    return cpuLoad.divideBy(cpuLimit).multiplyBy(100);
+  }
+
+  private queryCpuLoad(): TimeInstantQuery<number> {
+    const targetFilter = LabelFilters.regex('target_name', `${this.params.sloTarget.name}.*`);
+
+    return this.metricsSource.getTimeSeriesSource()
+      .select<number>('polaris', 'composed_metrics_polaris_slo_cloud_github_io_v1_cpu_load')
+      .filterOnLabel(targetFilter)
+      .sumByGroup();
+  }
+
+  private queryCpuLimit(): TimeInstantQuery<number> {
+    const resourceFilter = LabelFilters.equal('resource', 'cpu');
+    const podFilter = LabelFilters.regex('pod', `${this.params.sloTarget.name}.*`);
+
+    return this.metricsSource.getTimeSeriesSource()
+      .select<number>('kube', 'pod_container_resource_limits')
+      .filterOnLabel(podFilter)
+      .filterOnLabel(resourceFilter)
+      .minByGroup()
+      .multiplyBy(this.queryReplicas());
+  }
+
+  private queryReplicas(): TimeInstantQuery<number> {
+    const deploymentFilter = LabelFilters.equal('deployment', this.params.sloTarget.name);
+
+    return this.metricsSource.getTimeSeriesSource()
+      .select<number>('kube', 'deployment_spec_replicas')
+      .filterOnLabel(deploymentFilter)
+      .minByGroup();
   }
 }
