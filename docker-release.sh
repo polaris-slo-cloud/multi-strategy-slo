79749e3034a0f653a95ee5eb9d08297518c32a61
István Kiss <kissisti98@outlook.com>
2023-08-10 15:19:13 +0200

    cleanup

--- docker-release.sh
+++ docker-release.sh
@@ -7,14 +7,10 @@ fi
 
 for i in $APPS
 do
-  polaris-cli docker-build "$i" &
+  polaris-cli docker-build "$i"
 done
-
-wait < <(jobs -p)
 
 for i in $APPS
 do
-  docker push "stvnkiss/$i" &
+  docker push "stvnkiss/$i"
 done
-
-wait < <(jobs -p)
